ab2f9e5cd8ba6d287169d572a5a11d514141cb79
hdmamin <hmamin2@dons.usfca.edu>
2025-11-08 23:55:20 -0800

    save files during labeling

--- aeon/aeon/labeling.py
+++ aeon/aeon/labeling.py
@@ -19,7 +19,7 @@ from aeon.config import PROJECT_ROOT
 from aeon.logger import logger
 from aeon.prompt import Prompt
 from aeon.secrets import SecretManager
-from aeon.utils import timestamp
+from aeon.utils import timestamp, git_hash, timer
 
 
 class LLMLabeler:
@@ -36,33 +36,43 @@ class LLMLabeler:
         SecretManager().set_secrets()
         self.prompt = Prompt(prompt, **kwargs)
         self.client = OpenAI()
-        self.dir = Path(parent_dir)/prompt/timestamp()
-        self.dir.mkdir(parents=True, exist_ok=False)
+        self.parent_dir = Path(parent_dir)
+        # Will set this in `label` method.
+        self.output_dir = None
 
     def label(self, df: pd.DataFrame, max_workers: int = 15) -> dict:
+        self.output_dir = self.parent_dir/f"{self.prompt.name}/{timestamp()}-{git_hash()}"
+        self.output_dir.mkdir(parents=True, exist_ok=False)
+
         missing_vars = set(self.prompt.variables) - set(df.columns)
         if missing_vars:
             raise ValueError(f"df is missing variable(s): {missing_vars}")
 
         futures = []
         responses = []
+        # Pre-fill with dummy values mostly to illustrate to user what values to expect.
         results = {
-            "completed": True
+            "completed": True,
+            "duration_seconds": 0.0,
+            "df": None,
+            "n_errors": 0,
         }
         rows = df[self.prompt.variables].to_dict(orient='records')
         progress_bar = tqdm(total=len(rows), desc="Labeling rows")
-        with ThreadPoolExecutor(max_workers=max_workers) as executor:
-            try:
-                for i, row in enumerate(rows):
-                    futures.append(executor.submit(self._label_one_row, i, **row))
-                for future in as_completed(futures):
-                    responses.append(future.result())
-                    progress_bar.update(1)  
-            except KeyboardInterrupt:
-                logger.info("Canceling labeling job. Previously launched API calls will still run.")
-                results["completed"] = False
-                for future in futures:
-                    future.cancel()
+        with timer() as time:
+            with ThreadPoolExecutor(max_workers=max_workers) as executor:
+                try:
+                    for i, row in enumerate(rows):
+                        futures.append(executor.submit(self._label_one_row, i, **row))
+                    for future in as_completed(futures):
+                        responses.append(future.result())
+                        progress_bar.update(1)  
+                except KeyboardInterrupt:
+                    logger.info("Canceling labeling job. Previously launched API calls will still run.")
+                    results["completed"] = False
+                    for future in futures:
+                        future.cancel()
+        results["duration_seconds"] = time["duration"]
 
         df_labeled = pd.DataFrame(results)
         df_labeled = df_labeled.sort_values("i", ascending=True).reset_index(drop=True)
@@ -71,30 +81,38 @@ class LLMLabeler:
         # the list. Could standardize this and always return a list, even when unnecessary api could
         # always return a list of len 1. Or just leave as is? Or infer. idk
         df_labeled["response_parsed"] = df_labeled["response"].apply(
-            lambda x: {} if x is None else json.loads(x.choices[0].message.content)
+            lambda x: {} if x is None else json.loads(x["choices"][0]["message"]["content"])
         )
+        results["n_errors"] = df_labeled.shape[0] - df_labeled.success.sum()
         results["df"] = df_labeled
         return results
 
-    # TODO: maybe save this somewhere as well in case job fails late. Still determining whether it's
-    # fine to do here or if that will slow things down much. Could also save in `label`, e.g.
-    # every n'th iteration in as_completed (perhaps keeping separate queues of saved and unsaved)
-    # or having another thread running in the bg saving periodically (maybe queue is also useful
-    # here)
     def _label_one_row(self, i: int, **kwargs) -> dict:
+        """
+        Parameters
+        ----------
+        i : int
+            Row index. Used for error messages and to ensure we can recover original row order in
+            results.
+        """
         res = {
             "index": i,
             "success": True,
             "error": "",
             "response": None,
+            "api_kwargs": {},
         }
+        # TODO: more granular error messages for troubleshooting? Or maybe the fact that the error
+        # will naturally be different depending on what fails is sufficient.
         try:
-            api_kwargs = self.prompt.kwargs(**kwargs)
-            res["response"] = self.client.chat.completions.parse(
-                **api_kwargs
-            )
+            res["api_kwargs"] = self.prompt.kwargs(**kwargs)
+            response = self.client.chat.completions.parse(**res["api_kwargs"])
+            res["response"] = response.model_dump_json()
         except Exception as e:
             logger.error("row {i} failed with error: {e}")
             res["success"] = False
             res["error"] = traceback.format_exc()
+
+        with open(self.output_dir/f"{i}.json", "w") as f:
+            json.dump(res, f)
         return res
--- aeon/aeon/utils.py
+++ aeon/aeon/utils.py
@@ -1,5 +1,9 @@
+from contextlib import contextmanager
 from datetime import datetime
 import subprocess
+import time
+
+from aeon.logging import logger
 
 
 def timestamp() -> str:
@@ -7,5 +11,38 @@ def timestamp() -> str:
     return datetime.now().strftime("%Y.%m.%d_%H.%M.%S")
 
 
+def uncommitted_changes() -> bool:
+    """Check if there are any uncommitted changes in the current git repository.
+    This also returns True if there are unstaged changes because those are also uncommitted."
+
+    Note: this assumes we are using the editable install inside my larger aeon repo. If this was
+    installed norally in site-packages, this would not work as intended.
+    """
+    return bool(subprocess.check_output(["git", "status", "--porcelain"], text=True).strip())
+
+
 def git_hash() -> str:
+    """
+    Get the latest git commit hash from the current repo.
+
+    Note: this assumes we are using the editable install inside my larger aeon repo. If this was
+    installed norally in site-packages, this would not work as intended.
+    """
+    if uncommitted_changes():
+        logger.warning(
+            "Unstaged/uncommitted changes in git repository. You might want to commit them before "
+            "running."
+        )
     return subprocess.check_output(["git", "rev-parse", "HEAD"], text=True).strip()
+
+
+@contextmanager
+def timer(name: str = "BLOCK") -> dict:
+    """Time how long a block of code takes to execute.
+    """
+    try:
+        res = {"start": time.perf_counter()}
+        yield res
+    finally:
+        res["duration"] = time.perf_counter() - start
+        logger.info(f"[TIMER] {name} executed in {res['duration']:.3f} s.")
